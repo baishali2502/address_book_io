937fe4b6a365308f4fe6c5328bf55fe5a640851c
baishali2502 <baishali2502@gmail.com>
2023-12-14 18:07:53 +0530

    Added UC-1:Create a contact

--- Address_Book/src/Address_Book/Address_Book.java
+++ Address_Book/src/Address_Book/Address_Book.java
@@ -8,6 +8,39 @@ class AddressBook implements Serializable
 	// Maintains a list of contacts in the address-book
 	LinkedList<contact_node> head = new LinkedList<>();
 	
+	//---------------------------------------- UC-1 ---------------------------------------------
+
+		/*
+		 * @desc:This function creates a new contact
+		 * 
+		 * @returns:The new contact created
+		 * 
+		 * @params:none
+		 */
+		contact_node create() {
+			Scanner s = new Scanner(System.in);
+
+			System.out.print("Enter first-name : ");
+			String firstname = s.nextLine();
+			System.out.print("Enter last-name : ");
+			String lastname = s.nextLine();
+			System.out.println("Enter email : ");
+			String email = s.nextLine();
+			System.out.print("Enter address : ");
+			String address = s.nextLine();
+			System.out.print("Enter city : ");
+			String city = s.nextLine();
+			System.out.print("Enter state : ");
+			String state = s.nextLine();
+			System.out.print("Enter zip : ");
+			long zip = s.nextInt();
+			System.out.print("Enter phone-no. : ");
+			long phone = s.nextLong();
+
+			contact_node c = new contact_node(firstname, lastname, phone, email, address, city, state, zip);
+
+			return c;
+		}
 
     
 }
